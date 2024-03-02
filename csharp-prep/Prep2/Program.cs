7a4817ccac504ab9dca328ce5f8615b288a42201
adiquatech <quadrikolawoleadisa@gmail.com>
2024-03-02 09:39:54 +0100

    finished all prep assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,78 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        string percentage = Console.ReadLine();
+        int gradePercentage = int.Parse(percentage);
+        {
+            string letter = "";
+            string sign = "";
+
+            if (gradePercentage >= 90)
+            {
+                letter = "A";
+                if (gradePercentage == 100)
+                {
+                    sign = "";
+                }
+                else if (gradePercentage % 10 >= 7)
+                {
+                    sign = "";
+                }
+            }
+            else if (gradePercentage >= 80)
+            {
+                letter = "B";
+                if (gradePercentage % 10 >= 7)
+                {
+                    sign = "+";
+                }
+                else if (gradePercentage % 10 < 3)
+                {
+                    sign = "-";
+                }
+            }
+            else if (gradePercentage >= 70)
+            {
+                letter = "C";
+                if (gradePercentage % 10 >= 7)
+                {
+                    sign = "+";
+                }
+                else if (gradePercentage % 10 < 3)
+                {
+                    sign = "-";
+                }
+            }
+            else if (gradePercentage >= 60)
+            {
+                letter = "D";
+                if (gradePercentage % 10 >= 7)
+                {
+                    sign = "+";
+                }
+                else if (gradePercentage % 10 < 3)
+                {
+                    sign = "-";
+                }
+            }
+            else
+            {
+                letter = "F";
+                sign = "";
+            }
+
+            string finalGrade = $"{letter}{sign}";
+            Console.WriteLine($"Your letter grade is: {finalGrade}");
+
+            if (gradePercentage >= 70)
+            {
+                Console.WriteLine("Congratulations! You passed the course.");
+            }
+            else
+            {
+                Console.WriteLine("Better luck next time. Keep working hard!");
+            }
+        }
     }
 }
